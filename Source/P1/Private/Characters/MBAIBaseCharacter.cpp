af419947d960d30bf6bd645f6b722030daa05f88
membersaccount <192158855+membersaccount@users.noreply.github.com>
2025-02-24 14:02:43 +0900

    [update] AI select mode fix

--- Source/P1/Private/Characters/MBAIBaseCharacter.cpp
+++ Source/P1/Private/Characters/MBAIBaseCharacter.cpp
@@ -318,8 +318,7 @@ void AMBAIBaseCharacter::CheckForceLocationArrive()
 {
 	CalculatedTargetDistance = std::sqrt(
 		std::pow(ForceMoveLocation.X - AIInfo->InfoLocation.X, 2) +
-		std::pow(ForceMoveLocation.Y - AIInfo->InfoLocation.Y, 2) +
-		std::pow(ForceMoveLocation.Z - AIInfo->InfoLocation.Z, 2)
+		std::pow(ForceMoveLocation.Y - AIInfo->InfoLocation.Y, 2)
 	);
 
 	if (10.f > CalculatedTargetDistance)
